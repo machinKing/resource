56aa9cf14e67b415d612cffe0bf02796f8ee06be
machinKing <58395289+machinKing@users.noreply.github.com>
2025-01-23 19:39:14 +0800

    Update h2dyc.js

--- script/lhtj/h2dyc.js
+++ script/lhtj/h2dyc.js
@@ -233,11 +233,11 @@ async function getCookie() {
         if ($request && $request.method === 'OPTIONS') return;
 
         const body = ObjectKeys2LowerCase($request.body);
-        if (!body.get('Header').get('Token')) throw new Error("获取Cookie错误，值为空");
+        if (!body.get('header').get('token')) throw new Error("获取Cookie错误，值为空");
 
         const newData = {
             "userName": '微信用户',
-            'token': body.get('Header').get('Token')
+            'token': body.get('header').get('token')
         }
         const index = userCookie.findIndex(e => e.token == newData.token);
         index !== -1 ? userCookie[index] = newData : userCookie.push(newData);
