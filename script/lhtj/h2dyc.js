3061e49134920306d2bb0dc2fcc92560352607aa
machinKing <58395289+machinKing@users.noreply.github.com>
2025-01-24 10:36:53 +0800

    Update h2dyc.js

--- script/lhtj/h2dyc.js
+++ script/lhtj/h2dyc.js
@@ -24,9 +24,11 @@ hostname = gw2c-hw-open.longfor.com
 6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
 7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
 */
-const $ = new Env("龙湖天街");
+const $ = new Env("龙湖天街dyc");
 const ckName = "lhtj_data";
 const userCookie = $.toObj($.isNode() ? process.env[ckName] : $.getdata(ckName)) || [];
+const bodyname = "token";
+const body = {};
 //notify
 const notify = $.isNode() ? require('./sendNotify') : '';
 $.notifyMsg = []
@@ -233,7 +235,8 @@ async function getCookie() {
         if ($request && $request.method === 'OPTIONS') return;
 
         const body = ObjectKeys2LowerCase($request.body);
-        $.log(`${body}`)
+        $.setjson(bodyname, body);
+        $.msg($.body, `这个是body`, ``)
         if (!body['header']['token']) throw new Error("获取Cookie错误，值为空");
 
         const newData = {
